bc225177da814abd90970e65a6585b05f898c661
Gleb Churkin <gleb.churkin48@gmail.com>
2022-04-19 19:20:47 +0300

    First commit

---
+++ generator/views.py
@@ -0,0 +1,35 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+def home(request):
+    return render(request, 'generator/home.html') 
+    
+
+def description(request):
+    return render(request, 'generator/description.html')
+
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIGKLMNOPQRSTWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    
+    return render(request, 'generator/password.html', {'password':thepassword})
+
+
+
+    
